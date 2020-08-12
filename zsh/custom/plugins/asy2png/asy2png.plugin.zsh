5e8231524e00a320c316eba8b6f08abe27029fd5
Jeff LANCE <jeff.lance@mala.fr>
2020-08-12 17:50:39 +0200

    delete zsh files

--- zsh/custom/plugins/asy2png/asy2png.plugin.zsh
+++
@@ -1,5 +0,0 @@
-function asy2png {
-  asy "$1"
-  filename=$(basename -s .asy "$1")
-  rsvg-convert "$filename.svg" -o $(echo "$filename.svg" | sed -e 's/svg$/png/')
-}
--- zsh/custom/plugins/trash/trash.plugin.zsh
+++
@@ -1,7 +0,0 @@
-function trash {
-  if [[ -z "$1" ]]; then
-    trash-list
-  else
-    trash-put "$@"
-  fi
-}
