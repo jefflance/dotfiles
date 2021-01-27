8d1fcec183c8b472adbecc4829a4277cf7c32c62
Jeff LANCE <jeff.lance@mala.fr>
2021-01-27 22:38:26 +0100

    update gtk-theme-collections

--- helpers/install_gtk-theme-collections.sh
+++ helpers/install_gtk-theme-collections.sh
@@ -6,9 +6,9 @@ if [[ ! -d "$GTK_THEMES_DIR" ]]; then
     mkdir -p "$GTK_THEMES_DIR"
 fi
 
-if [[ ! -d "$GTK_THEMES_DIR/gtk-theme-collections" ]]; then
-    git clone https://github.com/addy-dclxvi/gtk-theme-collections.git "$GTK_THEMES_DIR/gtk-theme-collections"
+if [[ $(ls -A $GTK_THEMES_DIR) ]]; then
+    git -C "$GTK_THEMES_DIR" pull
 else
-    git -C "$GTK_THEMES_DIR/gtk-theme-collections" pull
+    git clone https://github.com/addy-dclxvi/gtk-theme-collections.git "$GTK_THEMES_DIR"
 fi
 
