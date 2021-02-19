22551e79feae77eddd6f7db3860630246979f0c0
Jeff LANCE <email@jefflance.me>
2021-02-19 23:23:31 +0100

    update and clean

---
+++ conf.d/zim/modules/sudo/init.zsh
@@ -0,0 +1,6 @@
+zle -N sudo-command-line
+
+# Defined shortcut keys: [Esc] [Esc]
+bindkey -M emacs '\e\e' sudo-command-line
+bindkey -M vicmd '\e\e' sudo-command-line
+bindkey -M viins '\e\e' sudo-command-line
---
+++ helpers/install_zsh.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ZIMDIR=${HOME}/.zim
+ZIMFILES="zimrc zshenv zlogin zshrc"
+
+echo "Creating directory"
+[[ ! -d ${ZIMDIR} ]] && mkdir ${ZIMDIR}
+
+echo "Getting Zim Framework"
+curl -fsSL https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh -o ${ZIMDIR}/zimfw.zsh
+
+echo "Treating links"
+for FILE in ${ZIMFILES}
+do
+  [[ -L ~/.${FILE} ]] && unlink ~/.${FILE}
+  ln -s ${PWD}/conf.d/zim/${FILE} ~/.${FILE}
+done
+
+echo "Making modules directory"
+[[ ! -d ${ZIMDIR}/modules ]] && mkdir ${ZIMDIR}/modules
+
+echo "Populating modules"
+for MOD in conf.d/zim/modules/*
+do
+  DIR=$(basename ${MOD})
+  [[ -L ${ZIMDIR}/modules/${DIR} ]] && unlink ${ZIMDIR}/modules/${DIR}
+  ln -s ${PWD}/conf.d/zim/modules/${DIR} ${ZIMDIR}/modules/${DIR}
+done
+
+echo "Installing Zim Framework"
+$(which zsh) ${ZIMDIR}/zimfw.zsh install
+
+exit 0
