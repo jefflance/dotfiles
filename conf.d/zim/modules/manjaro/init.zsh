edc14061308586ee037d7e2fe41c9d5dbc07d843
Jeff LANCE <email@jefflance.me>
2021-03-03 17:40:34 +0100

    change zim personal aliases

---
+++ conf.d/zim/modules/manjaro/init.zsh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+# File              : init.zsh
+# Author            : Jeff LANCE <email@jefflance.me>
+# Date              : 03.03.2021
+# Last Modified Date: 03.03.2021
+# Last Modified By  : Jeff LANCE <email@jefflance.me>
+#
+# Manjaro Shortcuts and Aliases
+#
+
+
+#
+# pamac Operations
+#
+
+alias pamse='pamac search'              # search through apt software package
+alias pamin='pamac info'                # show info about a package
+alias pamud='pamac update'              # update packages database
+alias pamug='pamac upgrade'             # upgrade packages
+alias pamin='pamac install'             # install package
+alias pamre='pamac remove'              # removes packages
+alias pamudg='pamud && pamug'
+
+
+#
+#  pkgfile Operations
+#
+
+alias pksf='pkgfile'                    # search file in packages
+
+
