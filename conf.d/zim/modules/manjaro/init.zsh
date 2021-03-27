87a142400de6786e5b3327ff062ad0e73d4d2c77
Jeff LANCE <email@jefflance.me>
2021-03-27 17:21:59 +0100

    update

--- conf.d/zim/modules/manjaro/init.zsh
+++ conf.d/zim/modules/manjaro/init.zsh
@@ -13,14 +13,26 @@
 # pamac Operations
 #
 
-alias pasea='pamac search'                # search through apt software package
+alias pasea='pamac search'                # search through package repositories
 alias painf='pamac info'                  # show info about a package
 alias pau='pamac update'                  # update packages database
 alias paU='pamac upgrade'                 # upgrade packages
 alias pain='pamac install'                # install package
-alias parem='pamac remove'                # removes packages
+alias parem='pamac remove'                # remove package
 alias pauU='pamud && pamug'
-alias pareo='parem -o'                    # removes orphan packages
+alias pareo='parem -o'                    # remove orphan packages
+
+
+#
+# pacman Operations
+#
+
+alias pacss='sudo pacman -Ss'                  # search through package repositories
+alias pacud='sudo pacman -Syu'                 # update system (and install package)
+alias pacin='sudo pacman -S'                   # install package
+alias pacrm='sudo pacman -R'                   # remove package
+alias paclo='sudo pacman -Qdt'                 # list orphans
+alias pacro='sudo pacman -Rs $(pacman -Qdt)'   # remove orphans
 
 
 #
