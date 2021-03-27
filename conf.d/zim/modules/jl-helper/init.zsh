c26998d583e9b81b37a923d267495e01e5a9f042
Jeff LANCE <email@jefflance.me>
2021-03-27 17:01:37 +0100

    add aliases

--- conf.d/zim/modules/jl-helper/init.zsh
+++ conf.d/zim/modules/jl-helper/init.zsh
@@ -4,10 +4,17 @@
 
 
 #
+# Admin
+#
+
+alias udvrld='sudo udevadm control --reload-rules && sudo udevadm trigger'
+
+
+#
 # Editors
 #
 
-alias vim='nvim'                         # redirect vim to nvim 
+alias vim='nvim'                         # redirect vim to nvim
 
 
 #
@@ -18,13 +25,14 @@ alias md='mkdir -p'                      # mkdir alias
 alias open='xdg-open'                    # open files
 alias ls='lsd'                           # pretty ls
 alias youtube-dl='youtube-dl -f best'    # youtube-dl best download
+alias xcolor='xcolor | xclip -i'         # xcolor
 
 
 #
 # Shortcuts
 #
 
-hash -d crs="${HOME}/Cours"              # cours  
+hash -d crs="${HOME}/Cours"              # cours
 hash -d doc="${HOME}/Documents"          # documents
 hash -d dot="${HOME}/dotfiles"           # dotfiles
 
--- conf.d/zim/modules/manjaro/init.zsh
+++ conf.d/zim/modules/manjaro/init.zsh
@@ -13,19 +13,20 @@
 # pamac Operations
 #
 
-alias pamsr='pamac search'              # search through apt software package
-alias paminf='pamac info'               # show info about a package
-alias pamud='pamac update'              # update packages database
-alias pamug='pamac upgrade'             # upgrade packages
-alias pamin='pamac install'             # install package
-alias pamre='pamac remove'              # removes packages
-alias pamudg='pamud && pamug'
+alias pasea='pamac search'                # search through apt software package
+alias painf='pamac info'                  # show info about a package
+alias pau='pamac update'                  # update packages database
+alias paU='pamac upgrade'                 # upgrade packages
+alias pain='pamac install'                # install package
+alias parem='pamac remove'                # removes packages
+alias pauU='pamud && pamug'
+alias pareo='parem -o'                    # removes orphan packages
 
 
 #
 #  pkgfile Operations
 #
 
-alias pamsf='pkgfile'                    # search file in packages
+alias pasf  ='pkgfile'                    # search file in packages
 
 
